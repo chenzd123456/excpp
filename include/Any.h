1192ad0681ce0dbdfd80671f2a68599dca919857
chenzd123456 <chenzd123456@gmail.com>
2023-05-10 16:05:25 +0800

    add Any

---
+++ include/Any.h
@@ -0,0 +1,211 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
+/**
+ * @class Any
+ * @brief A class for storing any type of data.
+ */
+class Any {
+  public:
+    enum Type {
+        UINT8,
+        UINT16,
+        UINT32,
+        UINT64,
+        INT8,
+        INT16,
+        INT32,
+        INT64,
+        FLOAT,
+        DOUBLE,
+        CHAR,
+        STRING,
+        BUFFER
+    };
+
+    /**
+     * @brief Constructor for Any class, takes a uint8_t value.
+     * @param value The value to be stored.
+     */
+    Any(uint8_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a uint16_t value.
+     * @param value The value to be stored.
+     */
+    Any(uint16_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a uint32_t value.
+     * @param value The value to be stored.
+     */
+    Any(uint32_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a uint64_t value.
+     * @param value The value to be stored.
+     */
+    Any(uint64_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a int8_t value.
+     * @param value The value to be stored.
+     */
+    Any(int8_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a int16_t value.
+     * @param value The value to be stored.
+     */
+    Any(int16_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a int32_t value.
+     * @param value The value to be stored.
+     */
+    Any(int32_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a int64_t value.
+     * @param value The value to be stored.
+     */
+    Any(int64_t value);
+
+    /**
+     * @brief Constructor for Any class, takes a float value.
+     * @param value The value to be stored.
+     */
+    Any(float value);
+
+    /**
+     * @brief Constructor for Any class, takes a double value.
+     * @param value The value to be stored.
+     */
+    Any(double value);
+
+    /**
+     * @brief Constructor for Any class, takes a char value.
+     * @param value The value to be stored.
+     */
+    Any(char value);
+
+    /**
+     * @brief Constructor for Any class, takes a std::string value.
+     * @param value The value to be stored.
+     */
+    Any(const std::string &value);
+
+    /**
+     * @brief Constructor for Any class, takes a std::vector<char> value.
+     * @param value The value to be stored.
+     */
+    Any(const std::vector<char> &value);
+
+    /**
+     * @brief Destructor for Any class.
+     */
+    ~Any();
+
+    /**
+     * @brief Get the type of the stored value.
+     * @return The type of the stored value.
+     */
+    Type getType() const;
+
+    /**
+     * @brief Get the stored value as a uint8_t.
+     * @return The stored value as a uint8_t.
+     */
+    uint8_t asUInt8() const;
+
+    /**
+     * @brief Get the stored value as a uint16_t.
+     * @return The stored value as a uint16_t.
+     */
+    uint16_t asUInt16() const;
+
+    /**
+     * @brief Get the stored value as a uint32_t.
+     * @return The stored value as a uint32_t.
+     */
+    uint32_t asUInt32() const;
+
+    /**
+     * @brief Get the stored value as a uint64_t.
+     * @return The stored value as a uint64_t.
+     */
+    uint64_t asUInt64() const;
+
+    /**
+     * @brief Get the stored value as a int8_t.
+     * @return The stored value as a int8_t.
+     */
+    int8_t asInt8() const;
+
+    /**
+     * @brief Get the stored value as a int16_t.
+     * @return The stored value as a int16_t.
+     */
+    int16_t asInt16() const;
+
+    /**
+     * @brief Get the stored value as a int32_t.
+     * @return The stored value as a int32_t.
+     */
+    int32_t asInt32() const;
+
+    /**
+     * @brief Get the stored value as a int64_t.
+     * @return The stored value as a int64_t.
+     */
+    int64_t asInt64() const;
+
+    /**
+     * @brief Get the stored value as a float.
+     * @return The stored value as a float.
+     */
+    float asFloat() const;
+
+    /**
+     * @brief Get the stored value as a double.
+     * @return The stored value as a double.
+     */
+    double asDouble() const;
+
+    /**
+     * @brief Get the stored value as a char.
+     * @return The stored value as a char.
+     */
+    char asChar() const;
+
+    /**
+     * @brief Get the stored value as a std::string.
+     * @return The stored value as a std::string.
+     */
+    std::string asString() const;
+
+    /**
+     * @brief Get the stored value as a std::vector<char>.
+     * @return The stored value as a std::vector<char>.
+     */
+    std::vector<char> asBuffer() const;
+
+  private:
+    Type type;
+    union {
+        uint8_t uint8Value;
+        uint16_t uint16Value;
+        uint32_t uint32Value;
+        uint64_t uint64Value;
+        int8_t int8Value;
+        int16_t int16Value;
+        int32_t int32Value;
+        int64_t int64Value;
+        float floatValue;
+        double doubleValue;
+        char charValue;
+        std::string *stringValue;
+        std::vector<char> *bufferValue;
+    };
+};
---
+++ src/Any.cpp
@@ -0,0 +1,65 @@
+#include "Any.h"
+
+Any::~Any() {
+    if (type == STRING) {
+        delete stringValue;
+    } else if (type == BUFFER) {
+        delete bufferValue;
+    }
+}
+
+Any::Type Any::getType() const { return type; }
+
+uint8_t Any::asUInt8() const { return uint8Value; }
+
+uint16_t Any::asUInt16() const { return uint16Value; }
+
+uint32_t Any::asUInt32() const { return uint32Value; }
+
+uint64_t Any::asUInt64() const { return uint64Value; }
+
+int8_t Any::asInt8() const { return int8Value; }
+
+int16_t Any::asInt16() const { return int16Value; }
+
+int32_t Any::asInt32() const { return int32Value; }
+
+int64_t Any::asInt64() const { return int64Value; }
+
+float Any::asFloat() const { return floatValue; }
+
+double Any::asDouble() const { return doubleValue; }
+
+char Any::asChar() const { return charValue; }
+
+std::string Any::asString() const { return *stringValue; }
+
+std::vector<char> Any::asBuffer() const { return *bufferValue; }
+
+Any::Any(uint8_t value) : type(UINT8), uint8Value(value) {}
+
+Any::Any(uint16_t value) : type(UINT16), uint16Value(value) {}
+
+Any::Any(uint32_t value) : type(UINT32), uint32Value(value) {}
+
+Any::Any(uint64_t value) : type(UINT64), uint64Value(value) {}
+
+Any::Any(int8_t value) : type(INT8), int8Value(value) {}
+
+Any::Any(int16_t value) : type(INT16), int16Value(value) {}
+
+Any::Any(int32_t value) : type(INT32), int32Value(value) {}
+
+Any::Any(int64_t value) : type(INT64), int64Value(value) {}
+
+Any::Any(float value) : type(FLOAT), floatValue(value) {}
+
+Any::Any(double value) : type(DOUBLE), doubleValue(value) {}
+
+Any::Any(char value) : type(CHAR), charValue(value) {}
+
+Any::Any(const std::string &value)
+    : type(STRING), stringValue(new std::string(value)) {}
+
+Any::Any(const std::vector<char> &value)
+    : type(BUFFER), bufferValue(new std::vector<char>(value)) {}
